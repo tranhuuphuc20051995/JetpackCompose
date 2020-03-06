963f4a2cf4cfb1d054e4a746e77d883c5f7ec0fe
phucthneolab <phuc.th@neo-lab.vn>
2020-03-06 17:00:39 +0700

    Demo button, demo height space, get vector from drawable

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,33 +1,87 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
-import androidx.ui.core.Text
-import androidx.ui.core.setContent
-import androidx.ui.material.MaterialTheme
-import androidx.ui.tooling.preview.Preview
+import androidx.compose.unaryPlus
+import androidx.ui.core.*
+import androidx.ui.foundation.shape.border.Border
+import androidx.ui.foundation.shape.corner.RoundedCornerShape
+import androidx.ui.graphics.Color
+import androidx.ui.graphics.vector.DrawVector
+import androidx.ui.layout.*
+import androidx.ui.material.*
+import androidx.ui.res.vectorResource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
-                Greeting("Android")
-            }
+            JCButton()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun JCButton() {
+    MaterialTheme {
+        Column {
+            //Normal button
+            Button(
+                text = "Button",
+                onClick = {
+
+                }
+            )
+            HeightSpacer(height = 10.dp)
+            //Custom style button
+            Button(
+                text = "Button",
+                onClick = {
+
+                },
+                style = OutlinedButtonStyle(
+                    color = Color.White,
+                    contentColor = Color.DarkGray,
+                    border = androidx.ui.foundation.shape.border.Border(Color.DarkGray, Dp(1f)),
+                    elevation = Dp(1f)
+                )
+            )
+            HeightSpacer(height = 10.dp)
+            //Button custom
+            Button(
+                children = {
+                    Column {
+                        Container(
+                            width = 50.dp,
+                            height = 50.dp
+                        ) {
+                            VectorImage(id = R.drawable.ic_baseline_add_24)
+                        }
+                        Text(text = "Hello")
+                        Text(text = "Button")
+                    }
+                },
+                style = OutlinedButtonStyle(
+                    Border(color = Color.Red, width = 1.dp),
+                    shape = RoundedCornerShape(50), //50% percent
+                    contentColor = Color.Red
+                )
+            )
+        }
+    }
 }
 
-@Preview
 @Composable
-fun DefaultPreview() {
-    MaterialTheme {
-        Greeting("Android")
+fun VectorImage(@DrawableRes id: Int, tintColor: Color = Color.Blue) {
+    val vector = +vectorResource(id)
+    WithDensity {
+        Container(
+            width = 50.dp,
+            height = 50.dp
+        ) {
+            DrawVector(vectorImage = vector, tintColor = tintColor)
+        }
     }
 }
